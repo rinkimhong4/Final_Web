20e7f5b77b375d2abac4e8a956c36f5e3f6f276a
Rirn Kimhong <rirnkimhong@Rirns-MacBook-Air.local>
2025-05-03 23:52:11 +0700

    fix product form

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let title = document.getElementById("title");
+let price = document.getElementById("price");
+let taxes = document.getElementById("taxes");
+let discount = document.getElementById("discount");
+let qrImageUrl = document.getElementById("qrImageUrl");
+let total = document.getElementById("total");
+let count = document.getElementById("count");
+let category = document.getElementById("category");
+let btnCancel = document.getElementById("btnCancel");
+let btnAdd = document.getElementById("btnAdd");
+let shirtForm = document.getElementById("shirtForm");
+let tbody = document.getElementById("tbody");
+
+console.log(
+  title,
+  price,
+  taxes,
+  discount,
+  qrImageUrl,
+  total,
+  count,
+  category,
+  btnCancel,
+  btnAdd,
+  shirtForm,
+  tbody
+);
