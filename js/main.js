c56f4d2fdf6641587a53c98f9f52db0b8fc846d3
Rirn Kimhong <rirnkimhong@Rirns-MacBook-Air.local>
2025-05-05 01:20:50 +0700

    fix

--- js/main.js
+++ js/main.js
@@ -1,29 +1,38 @@
 let shirtForm = document.getElementById("shirtForm");
-let titleInput = document.getElementById("title");
-let priceInput = document.getElementById("price");
-let taxesInput = document.getElementById("taxes");
-let discountInput = document.getElementById("discount");
-let qrImageUrlInput = document.getElementById("qrImageUrl");
-let countInput = document.getElementById("count");
-let categoryInput = document.getElementById("category");
-let totalSpan = document.getElementById("total");
-let brandInput = document.getElementById("brand");
+let title = document.getElementById("title");
+let price = document.getElementById("price");
+let taxes = document.getElementById("taxes");
+let discount = document.getElementById("discount");
+let qrImageUrl = document.getElementById("qrImageUrl");
+let total = document.getElementById("total");
+let count = document.getElementById("count");
+let category = document.getElementById("category");
+let brand = document.getElementById("brand");
 
-let tbody = document.getElementById("tbody");
+console.log("shirtForm:", shirtForm);
+console.log("title:", title);
+console.log("price:", price);
+console.log("taxes:", taxes);
+console.log("discount:", discount);
+console.log("qrImageUrl:", qrImageUrl);
+console.log("total:", total);
+console.log("count:", count);
+console.log("category:", category);
+console.log("brand:", brand);
 
 let btnAdd = document.getElementById("btnAdd");
 let btnCancel = document.getElementById("btnCancel");
+let tbody = document.getElementById("tbody");
 
-let editModal = new bootstrap.Modal(document.getElementById("editModal"));
-let editForm = document.getElementById("editForm");
-let editIdInput = document.getElementById("editId");
-let editTitleInput = document.getElementById("editTitle");
-let editPriceInput = document.getElementById("editPrice");
-let editDiscountInput = document.getElementById("editDiscount");
-let editCountInput = document.getElementById("editCount");
-let editCategoryInput = document.getElementById("editCategory");
-let editQrImageUrlInput = document.getElementById("editQrImageUrl");
+let successToast = document.getElementById("successToast");
+let errorToast = document.getElementById("errorToast");
+
+let editId = document.getElementById("editId");
+let editTitle = document.getElementById("editTitle");
+let editPrice = document.getElementById("editPrice");
+let editDiscount = document.getElementById("editDiscount");
+let editCount = document.getElementById("editCount");
+let editCategory = document.getElementById("editCategory");
+let editBrand = document.getElementById("editBrand");
+let editQrImageUrl = document.getElementById("editQrImageUrl");
 let btnSaveEdit = document.getElementById("btnSaveEdit");
-
-let successToast = new bootstrap.Toast(document.getElementById("successToast"));
-let errorToast = new bootstrap.Toast(document.getElementById("errorToast"));
